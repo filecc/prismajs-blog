7df0b285b43ce252c42ed07b7d8d9743a0754319
filecc <fil.gia@live.it>
2023-11-22 14:58:28 +0100

    all posts

--- get-posts.js
+++ get-posts.js
@@ -1,11 +1,7 @@
 (async () => {
     const prisma = require('./lib/prisma-client')
     
-    prisma.post.findUnique({
-        where: {
-            slug: 'prisma-makes-databases-easy-01'
-        }
-    }).then((result) => {
+    prisma.post.findMany().then((result) => {
         console.log(result)
     }).catch((error) => {
         console.log(error)
